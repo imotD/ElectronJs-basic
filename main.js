9e5e25a3a17d6dbdc8c4de32efbf00b7bb792ff5
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-11-30 16:00:24 +0700

    add menu developet tools

--- main.js
+++ main.js
@@ -83,3 +83,20 @@ if (process.platform == "darwin") {
 }
 
 // Add developers tools item if not in prod
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
