330e007580cd5b1e62b5982bac01ed1935e821fb
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-11-30 15:22:52 +0700

    git init

---
+++ main.js
@@ -0,0 +1,32 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
+
+// * Listen for app to be ready
+app.on("ready", function() {
+  //* create new window
+  mainWindow = new BrowserWindow({});
+  //* load html into window
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  //* Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //* insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+//* create menu template
+const mainMenuTemplate = [
+  {
+    label: "File"
+  }
+];
