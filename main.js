57f795bd24f85253d00cf584742bb509659351fd
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-11-30 15:53:53 +0700

    add submenu

--- main.js
+++ main.js
@@ -5,32 +5,63 @@ const path = require("path");
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
-// * Listen for app to be ready
+//  Listen for app to be ready
 app.on("ready", function() {
-  //* create new window
+  // create new window
   mainWindow = new BrowserWindow({});
-  //* load html into window
+  // load html into window
   mainWindow.loadURL(
     url.format({
-      pathname: path.join(__dirname, "index.html"),
+      pathname: path.join(__dirname, "mainWindow.html"),
       protocol: "file:",
       slashes: true
     })
   );
-  //* Build menu from template
+  // Quite app when closed
+  mainWindow.on("close", function() {
+    app.quit();
+  });
+
+  // Build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-  //* insert menu
+  // insert menu
   Menu.setApplicationMenu(mainMenu);
 });
 
-//* create menu template
+// Handle create add window
+function createAddWindow() {
+  // create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add Shopping List Item"
+  });
+  // load html into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Garbage collection handle
+  addWindow.on("close", function() {
+    addWindow = null;
+  });
+}
+
+// create menu template
 const mainMenuTemplate = [
   {
     label: "File",
     submenu: [
       {
-        label: "Add Item"
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: "Clear Items"
@@ -45,3 +76,10 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+// IF mac add empty object to menu
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
+
+// Add developers tools item if not in prod
