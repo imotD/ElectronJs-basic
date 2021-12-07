a1ede807b5e833af5d40e12b181910c891354024
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-12-07 17:30:12 +0700

    add design album

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,34 @@
 <template>
   <v-container>
-    <h1>Hai World</h1>
-    {{ data }}
+    <h1>My Album</h1>
+    <v-row class="py-10">
+      <v-col cols="4" v-for="n in 9" :key="n">
+        <v-hover v-slot="{ hover }">
+          <v-card :elevation="hover ? 16 : 2">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+            <v-fade-transition>
+              <v-overlay v-if="hover" absolute color="#036358">
+                <v-btn>See more info</v-btn>
+              </v-overlay>
+            </v-fade-transition>
+          </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -10,12 +37,13 @@ export default {
   name: "HelloWorld",
 
   data: () => ({
-    data: ""
+    data: "",
+    // overlay: false
   }),
   mounted() {
     this.$axios
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => this.data = response.data));
+      .then((response) => (this.data = response.data));
   }
 };
 </script>
